173a78309a6780ab9e0ed5d7f07c012c73311ffd
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-06 22:22:23 +0700

    Install

---
+++ src/Demo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Demo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello ReactJS</h1>
+        <p>Chào mừng đến với khóa học lập trình ReactJS</p>
+      </div>
+    )
+  }
+}
+
+export default Demo
