771b45cda5ddfd589fe421f67b1cf3d179508a9b
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-06 22:23:07 +0700

    Updates

---
+++ precache-manifest.b1251248274519cc2c91d97e94e90cfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b7511ea1af2b040852ab33dfb1f355",
+    "url": "/lesson01-helloreact/index.html"
+  },
+  {
+    "revision": "1a234e7f25442f1b565a",
+    "url": "/lesson01-helloreact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "00bff57293e3cc16abd8",
+    "url": "/lesson01-helloreact/static/js/2.ca7895ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lesson01-helloreact/static/js/2.ca7895ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a234e7f25442f1b565a",
+    "url": "/lesson01-helloreact/static/js/main.7909ce0f.chunk.js"
+  },
+  {
+    "revision": "230f01403a83a56f4678",
+    "url": "/lesson01-helloreact/static/js/runtime-main.ae531c84.js"
+  }
+]);
